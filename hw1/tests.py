fdbdfa091c2f51c93ec307e2abd645f0c0af7414
Jordan Boyd-Graber <jordanbg@gmail.com>
2014-08-15 12:28:22 -0600

    adding more tests

--- hw1/tests.py
+++ hw1/tests.py
@@ -8,13 +8,34 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_rhyme(self):
         self.assertEqual(self.ld.rhymes("dog", "bog"), True)
+        self.assertEqual(self.ld.rhymes("eleven", "seven"), True)
+        self.assertEqual(self.ld.rhymes("nine", "wine"), True)
+        self.assertEqual(self.ld.rhymes("dine", "fine"), True)
+        self.assertEqual(self.ld.rhymes("wine", "mine"), True)
+        self.assertEqual(self.ld.rhymes("dock", "sock"), True)
+        self.assertEqual(self.ld.rhymes("weigh", "fey"), True)
+        self.assertEqual(self.ld.rhymes("tree", "debris"), True)
+        self.assertEqual(self.ld.rhymes("niece", "peace"), True)
+        self.assertEqual(self.ld.rhymes("read", "need"), True)
+        
         self.assertEqual(self.ld.rhymes("dog", "cat"), False)
-
+        self.assertEqual(self.ld.rhymes("bagel", "sail"), False)
+        self.assertEqual(self.ld.rhymes("wine", "rind"), False)
+        self.assertEqual(self.ld.rhymes("failure", "savior"), False)        
+        self.assertEqual(self.ld.rhymes("cup", "duck"), False)
+                        
     def test_syllables(self):
         self.assertEqual(self.ld.num_syllables("dog"), 1)
         self.assertEqual(self.ld.num_syllables("asdf"), 1)
         self.assertEqual(self.ld.num_syllables("letter"), 2)
         self.assertEqual(self.ld.num_syllables("washington"), 3)
+        self.assertEqual(self.ld.num_syllables("dock"), 1)
+        self.assertEqual(self.ld.num_syllables("dangle"), 2)
+        self.assertEqual(self.ld.num_syllables("thrive"), 1)
+        self.assertEqual(self.ld.num_syllables("fly"), 1)
+        self.assertEqual(self.ld.num_syllables("placate"), 2)
+        self.assertEqual(self.ld.num_syllables("renege"), 2)
+        self.assertEqual(self.ld.num_syllables("reluctant"), 3)
 
     def test_examples(self):
 
@@ -40,9 +61,37 @@ nobody will read this
 i'll end this here poem potato
 """
 
+        d = """There was a young man named Wyatt
+whose voice was exceedingly quiet
+And then one day
+it faded away"""
+
+        e = """An exceedingly fat friend of mine,
+When asked at what hour he'd dine,
+Replied, "At eleven,     
+At three, five, and seven,
+And eight and a quarter past nine"""
+
+        f = """A limerick fan from Australia
+regarded his work as a failure:
+his verses were fine
+until the fourth line"""
+
+        g = """There was a young lady one fall
+Who wore a newspaper dress to a ball.
+The dress caught fire
+And burned her entire
+Front page, sporting section and all."""
+
+        h = "dog\ndog\ndog\ndog\ndog"
+
         self.assertEqual(self.ld.is_limerick(a), True)
         self.assertEqual(self.ld.is_limerick(b), False)
         self.assertEqual(self.ld.is_limerick(c), False)
-
+        self.assertEqual(self.ld.is_limerick(d), False)
+        self.assertEqual(self.ld.is_limerick(f), False)
+        self.assertEqual(self.ld.is_limerick(e), True)
+        self.assertEqual(self.ld.is_limerick(g), True)
+        
 if __name__ == '__main__':
     unittest.main()
