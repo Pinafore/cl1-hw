c3f56ce0d9cd02728238ccc7a2a0cf7edd0b5af0
Jordan Boyd-Graber <jordanbg@gmail.com>
2022-09-22 20:47:57 -0400

    Update wn_search.py

--- wn_logic/wn_search.py
+++ wn_logic/wn_search.py
@@ -1,15 +1,38 @@
+# Author: Jordan Boyd-Graber
+# Date: 22. Sept 2022
+# Homework Template: A bad searcher that tries to find a target synset in WordNet
+
 from nltk.corpus import wordnet as wn
 
 class Searcher:
+    """
+    Class to search WordNet through logical queries.
+    """
+    
     def __init__(self):
+        
+        # Feel free to add your own data members
         self._searched = {}
 
     def check_lemma(self, oracle, candidate):
+        """
+        Convenience method to check whether two synsets are the same by seeing if the oracle matches a lemma of this synset.
+        
+        Keyword Arguments:
+        oracle -- The oracle that can check whether the candidate matches
+        candidate -- The synset to check
+        """
         print("Searching %s" % str(candidate))        
         lemma = candidate.lemmas()[0]
         self._searched[candidate] = oracle.there_exists('lemmas', [lemma])
         
     def __call__(self, oracle):
+        """
+        Given an oracle, return the synset that the oracle has as its target.
+        
+        Keyword Arguments:
+        oracle -- The oracle being searched
+        """
 
         # Feel free to change the code within
         # --------------------------------------
