675c2ac32cf596ab49c18d00032f16d3e444db63
Jordan Boyd-Graber <jbg@boydgraber.org>
2024-08-27 16:01:44 -0400

    draft ready

--- bigram_lm/bigram_lm.py
+++ bigram_lm/bigram_lm.py
@@ -1,63 +1,112 @@
 from math import log, exp
 from collections import defaultdict
 import argparse
+import typing
 
 from numpy import mean
 
 import nltk
 from nltk import FreqDist
 from nltk.util import bigrams
-from nltk.tokenize import TreebankWordTokenizer
 
 kLM_ORDER = 2
 kUNK_CUTOFF = 3
 kNEG_INF = -1e6
 
-kSTART = "<s>"
-kEND = "</s>"
+kSTART = "<S>"
+kEND = "</S>"
 
+token = int | str
+
+def lower(str):
+    return str.lower()
+    
 def lg(x):
     return log(x) / log(2.0)
 
+def dict_sample(d: typing.Dict, cutoff: int =-1):
+    """
+    Sample a key from a dictionary using the values as probabilities
+    (unnormalized)
+
+    cutoff -- Give up after encountering this much probability mass; set to
+    something like 0.9 if sampling is taking too long.
+    """
+
+    #TODO(only for extra credit): Implement this function
+    
+    from random import random
+
+
+
 class BigramLanguageModel:
 
-    def __init__(self, unk_cutoff, jm_lambda=0.6, dirichlet_alpha=0.1,
-                 katz_cutoff=5, kn_discount=0.1, kn_concentration=1.0,
-                 tokenize_function=TreebankWordTokenizer().tokenize,
-                 normalize_function=lambda x: x.lower()):
+    def __init__(self, unk_cutoff: int, jm_lambda: float=0.6, dirichlet_alpha: float=0.1,
+                 kn_discount: float=0.1, kn_concentration: float=1.0,
+                 standardize_function: typing.Callable=lambda x: x.lower()):
+        """Initialize our bigram language model.
+
+        jm_lambda -- Parameter that controls interpolation between unigram and
+        bigram: this is the unigram weight.
+
+        dirichlet_alpha -- The pseudocount added to every observation (even
+        zero count ones) when estimating the probability (thus in total, we
+        add dirichlet_alpha times the vocabulary size)
+
+        kn_concentration -- How much we weight the backoff distribution (same
+        for all contexts) for Knesser-Ney, theta in the equation
+        
+        kn_discount -- How much we subtract from every count and then add to
+        the backoff distribution (same for all contexts) for Knesser-Ney,
+        delta in the equation.
+
+        standardize_function -- Function to turn words into a simplified base
+        form.
+
+        """
         self._unk_cutoff = unk_cutoff
         self._jm_lambda = jm_lambda
         self._dirichlet_alpha = dirichlet_alpha
-        self._katz_cutoff = katz_cutoff
-        self._kn_concentration = kn_concentration
         self._kn_discount = kn_discount
+        self._kn_concentration = kn_concentration
         self._vocab_final = False
+        self._vocab = set()
 
-        self._tokenizer = tokenize_function
-        self._normalizer = normalize_function
+        self._standardizer = standardize_function
         
-        # Add your code here!
+        self._training_counts = FreqDist()
 
-    def train_seen(self, word, count=1):
+        # Add your code here!        
+        # Bigram counts        
+
+        # Unigram counts
+
+        # Prefix counts
+
+    def train_seen(self, word: str, count: int=1):
         """
         Tells the language model that a word has been seen @count times.  This
         will be used to build the final vocabulary.
+
+        word -- The word we saw
+        count -- How many times we saw the word
         """
+        
         assert not self._vocab_final, \
             "Trying to add new words to finalized vocab"
 
-        # Add your code here!            
+        self._training_counts[word] += count
 
-    def tokenize(self, sent):
+            
+    def sample(self, sample_size: int) -> typing.Iterator[str]:
         """
-        Returns a generator over tokens in the sentence.  
-
-        You don't need to modify this code.
-        """
-        for ii in self._tokenizer(sent):
-            yield ii
+        Extra Credit: Generate from the language model
         
-    def vocab_lookup(self, word):
+        sample_size -- How many tokens to generate from the model
+        """
+        return ""                                                      
+            
+    def vocab_lookup(self, word: str) -> token:
         """
         Given a word, provides a vocabulary representation.  Words under the
         cutoff threshold shold have the same value.  All words with counts
@@ -66,58 +115,78 @@ class BigramLanguageModel:
         assert self._vocab_final, \
             "Vocab must be finalized before looking up words"
 
-        # Add your code here
-        return -1
+        if word in self._vocab:
+            return word
+        else:
+            return None
 
     def finalize(self):
         """
         Fixes the vocabulary as static, prevents keeping additional vocab from
         being added
         """
-
-        # You probably do not need to modify this code
+        
         self._vocab_final = True
+        self._vocab = set(x for x in self._training_counts if \
+                          self._training_counts[x] >= self._unk_cutoff)
+        self._vocab.add(kSTART)
+        self._vocab.add(kEND)
+        
+        # -------------------------------------------------------------------
+        # You may want to add code here because it's at this point you know
+        # the vocab size
+        # -------------------------------------------------------------------
 
-    def tokenize_and_censor(self, sentence):
+        assert self.vocab_lookup(kSTART) is not None, "Missing start"
+        assert self.vocab_lookup(kEND) is not None, "Missing end"
+        
+    def censor(self, sentence: typing.Iterable[str]) -> typing.Iterator[str] | typing.Iterator[int]:
         """
-        Given a sentence, yields a sentence suitable for training or
-        testing.  Prefix the sentence with <s>, replace words not in
-        the vocabulary with <UNK>, and end the sentence with </s>.
+        Given a sentence, yields a sentence suitable for training or testing.
+        Prefix the sentence with <s>, replace words not in the vocabulary with
+        <UNK>, and end the sentence with </s>.
 
-        You should not modify this code.
+        sentence -- The original sentence
         """
         yield self.vocab_lookup(kSTART)
-        for ii in self._tokenizer(sentence):
-            yield self.vocab_lookup(self._normalizer(ii))
+        for word in sentence:
+            yield self.vocab_lookup(self.standardize(word))
         yield self.vocab_lookup(kEND)
 
-
-    def normalize(self, word):
+    def standardize(self, word: str) -> int | str:
         """
-        Normalize a word
+        Standardize a word: if it's not in the vocabulary, replace it with the unknown token
 
-        You should not modify this code.
+        word -- The original word
         """
-        return self._normalizer(word)
+        if word is None:
+            return None
+        else:
+            return self._standardizer(word)
 
-
-    def mle(self, context, word):
+    def mle(self, context: token, word: token) -> float:
         """
-        Return the log MLE estimate of a word given a context.  If the
-        MLE would be negative infinity, use kNEG_INF
+        Return the log MLE estimate of a word given a context.  If the MLE would
+        be negative infinity, use kNEG_INF
+
+        word -- The word we compute the probability for
+        context -- The context that we condition on
         """
 
         # This initially return 0.0, ignoring the word and context.
-        # Modify this code to return the correct value.
+        # Modify this code to return the correct value.        
         return 0.0
 
-    def laplace(self, context, word):
+    def laplace(self, context: token, word: token):
         """
         Return the log MLE estimate of a word given a context.
+
+        word -- The word we compute the probability for
+        context -- The context that we condition on
         """
 
         # This initially return 0.0, ignoring the word and context.
-        # Modify this code to return the correct value.
+        # Modify this code to return the correct value.                    
         return 0.0
 
     def jelinek_mercer(self, context, word):
@@ -125,119 +194,155 @@ class BigramLanguageModel:
         Return the Jelinek-Mercer log probability estimate of a word
         given a context; interpolates context probability with the
         overall corpus probability.
+
+        word -- The word we compute the probability for
+        context -- The context that we condition on
+        """
+
+        # This initially return 0.0, ignoring the word and context.
+        # Modify this code to return the correct value.                
+        return 0.0
+
+        
+          
+    def kneser_ney(self, context: token, word: token) -> float:
+        """
+        Return the log probability of a word given a context given Kneser
+        Ney backoff, as approximated by the minimal seating assumption of the
+        Chinese restaurant process.
+
+        word -- The word we compute the probability for
+        context -- The context that we condition on
         """
         # This initially return 0.0, ignoring the word and context.
         # Modify this code to return the correct value.
         return 0.0
 
-    def kneser_ney(self, context, word):
-        """
-        Return the log probability of a word given a context given
-        Kneser Ney backoff
-        """
-        # This initially return 0.0, ignoring the word and context.
-        # Modify this code to return the correct value.
-        return 0.0
-
-    def dirichlet(self, context, word):
+    
+    def dirichlet(self, context: token, word: token) -> float:
         """
         Additive smoothing, assuming independent Dirichlets with fixed
         hyperparameter.
+
+        word -- The word we compute the probability for
+        context -- The context that we condition on
         """
         # This initially return 0.0, ignoring the word and context.
         # Modify this code to return the correct value.
         return 0.0
 
-    def add_train(self, sentence):
+    def vocab_size(self) -> int:
+        """
+        Return the size of the vocabulary.
+        """
+        return len(self._vocab)
+    
+    def add_train(self, sentence: typing.Iterable[str]):
         """
         Add the counts associated with a sentence.
+
+        sentence -- The sentence we extract the counts from
         """
 
-        # You'll need to complete this function, but here's a line of
-        # code that will hopefully get you started.
-        for context, word in bigrams(self.tokenize_and_censor(sentence)):
+        # You'll need to complete this function, but here's a line of code that
+        # will hopefully get you started.
+        for context, word in bigrams(self.censor(sentence)):
             None
+            # ---------------------------------------
 
-    def perplexity(self, sentence, method):
+    def perplexity(self, sentence: str, method: typing.Callable) -> float:
         """
-        Compute the perplexity of a sentence given a estimation method
+        Compute the perplexity of a sentence given an estimation technique
 
-        You do not need to modify this code.
+        sentence -- The sentence we compute the perplexity for
+        method -- The estimator function
         """
+
+        # You don't have to modify this function
         return 2.0 ** (-1.0 * mean([method(context, word) for context, word in \
-                                    bigrams(self.tokenize_and_censor(sentence))]))
+                                    bigrams(self.censor(sentence))]))
 
-    def sample(self, method, samples=25):
-        """
-        Sample words from the language model.
-        
-        @arg samples The number of samples to return.
-        """
-        # Modify this code to get extra credit.  This should be
-        # written as an iterator.  I.e. yield @samples times followed
-        # by a final return, as in the sample code.
 
-        for ii in xrange(samples):
-            yield ""
-        return
-
-# You do not need to modify the below code, but you may want to during
-# your "exploration" of high / low probability sentences.
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--jm_lambda", help="Parameter that controls " + \
-                           "interpolation between unigram and bigram",
+                           "interpolation between unigram and bigram: " + \
+                           "this is the unigram weight.",
                            type=float, default=0.6, required=False)
-    argparser.add_argument("--dir_alpha", help="Dirichlet parameter " + \
-                           "for pseudocounts",
+    argparser.add_argument("--dir_alpha", help="The pseudocount added to " + \
+                           "every observation (even zero count ones) when" + \
+                           "estimating the probability (thus in total, we" + \
+                           "add dirichlet_alpha times the vocabulary size)",
                            type=float, default=0.1, required=False)
     argparser.add_argument("--unk_cutoff", help="How many times must a word " + \
                            "be seen before it enters the vocabulary",
                            type=int, default=2, required=False)    
-    argparser.add_argument("--katz_cutoff", help="Cutoff when to use Katz " + \
-                           "backoff",
-                           type=float, default=0.0, required=False)
     argparser.add_argument("--lm_type", help="Which smoothing technique to use",
                            type=str, default='mle', required=False)
-    argparser.add_argument("--brown_limit", help="How many sentences to add " + \
-                           "from Brown",
+    argparser.add_argument("--train_limit", help="How many sentences to add " + \
+                           "from the training corpus.",
                            type=int, default=-1, required=False)
-    argparser.add_argument("--kn_discount", help="Kneser-Ney discount parameter",
-                           type=float, default=0.1, required=False)
-    argparser.add_argument("--kn_concentration", help="Kneser-Ney concentration parameter",
+    argparser.add_argument("--test_limit", help="How many test sentences from " + \
+                           "the test corpus (Gutenberg)", type=int, default=1, required=False)
+    argparser.add_argument("--kn_concentration", help="How much we weight the " + \
+                           "backoff distribution (same for all contexts) for " + \
+                           "Knesser-Ney, theta in the equation.",
                            type=float, default=1.0, required=False)
-    argparser.add_argument("--method", help="Which LM method we use",
-                           type=str, default='laplace', required=False)
+    argparser.add_argument("--kn_discount", help="How much we subtract from " + \
+                           "every count and then add to the backoff distribution " + \
+                           "(same for all contexts) for Knesser-Ney, delta in the " + \
+                           "equation.",
+                           type=float, default=0.1, required=False)
     
     args = argparser.parse_args()    
     lm = BigramLanguageModel(kUNK_CUTOFF, jm_lambda=args.jm_lambda,
                              dirichlet_alpha=args.dir_alpha,
-                             katz_cutoff=args.katz_cutoff,
-                             kn_concentration=args.kn_concentration,
                              kn_discount=args.kn_discount)
 
-    for ii in nltk.corpus.brown.sents():
-        for jj in lm.tokenize(" ".join(ii)):
-            lm.train_seen(lm._normalizer(jj))
+    for sent in nltk.corpus.brown.sents():
+        for word in sent:
+            lm.train_seen(lm.standardize(word))
 
     print("Done looking at all the words, finalizing vocabulary")
     lm.finalize()
 
     sentence_count = 0
-    for ii in nltk.corpus.brown.sents():
+    for sent in nltk.corpus.brown.sents():
         sentence_count += 1
-        lm.add_train(" ".join(ii))
+        lm.add_train(sent)
 
-        if args.brown_limit > 0 and sentence_count >= args.brown_limit:
+        if args.train_limit > 0 and sentence_count >= args.train_limit:
             break
 
     print("Trained language model with %i sentences from Brown corpus." % sentence_count)
-    assert args.method in ['kneser_ney', 'mle', 'dirichlet', \
-                           'jelinek_mercer', 'good_turing', 'laplace'], \
-      "Invalid estimation method"
 
-    sent = input()
-    while sent:
-        print("#".join(str(x) for x in lm.tokenize_and_censor(sent)))
-        print(lm.perplexity(sent, getattr(lm, args.method)))
-        sent = input()
+    # Build the test corpus
+    num_sentences = len(nltk.corpus.gutenberg.sents())
+
+    if args.test_limit > 0:
+        from random import sample
+        sentence_indices = sample(range(num_sentences), args.test_limit)
+    else:
+        from random import shuffle
+        sentence_indices = list(range(num_sentences))
+        shuffle(sentence_indices)        
+    
+    for method_name in ['kneser_ney', 'mle', 'dirichlet', 'jelinek_mercer', 'laplace']:
+        print("======================")
+        print("      %s" % method_name)
+        print("======================")        
+        sentence_count = 0
+        method = getattr(lm, method_name)
+
+        from random import shuffle
+        sentences = nltk.corpus.gutenberg.sents()
+
+        for sent_index in sentence_indices:
+            sent = sentences[sent_index]
+            original = list(sent)
+            censored = list(lm.censor(original))
+
+            for ii, jj, kk in zip([""] + original, censored, [0.0] + [method(censored[ii-1], censored[ii]) for ii in range(1, len(censored))]):
+                print("%10s\t%10s\t%03.4f" % (ii, jj, kk))
+            print("Perplexity: %0.4f" % lm.perplexity(censored, method))
+            print("----------------")
